a058de6df3b5a6d41f81416fd59e8dfbf6b6ee43
x-ning <ngx@ngx.wiki>
2017-05-07 15:59:56 +0800

    练习题

---
+++ day02/stu01.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+# -*- coding:utf-8 -*-
+# Author:X.Ning <ngx@ngx.wiki>
+name = " aleX "
+# a.
+name_a = name.strip()
+print(name_a)
+
+# b.
+name_b = name_a.startswith("al")
+print(name_b)
+
+# c.
+name_c = name_a.endswith('X')
+print(name_c)
+
+# d.
+name_d = name_a.replace("l","p")
+print(name_d)
+
+# e.
+name_e = name_a.partition("l")
+print(name_e)
+
+# f.
