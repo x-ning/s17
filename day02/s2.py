614c4ab3f498d584d02d7d767dde316327888221
x-ning <ngx@ngx.wiki>
2017-05-07 15:01:35 +0800

    14 str commit

---
+++ day02/s2.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+# -*- coding:utf-8 -*-
+# Author:X.Ning <ngx@ngx.wiki>
+
+#14. 是否是表示符（能否做变量使用【系统关键字也可以，但除外】）
+# name1 = 'x.ning'
+# name2 = 'User_name'
+# v1 = name1.isidentifier()
+# v2 = name2.isidentifier()
+# print(v1)
+# print(v2)
+
+#15. 是否全部为小写
+
