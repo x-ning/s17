80df0da14b0cbdd94e21b72c9f43ff8e56c93179
x-ning <ngx@ngx.wiki>
2017-05-07 17:49:21 +0800

    列表-练习题 range

---
+++ day02/s4.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python
+# -*- coding:utf-8 -*-
+# Author:X.Ning <ngx@ngx.wiki>
+
+# 3.x range不会立即生成
+for i in range(1,11):
+    print(i)
+
+# 步长
+for i in range(1,11,2):
+    print(i)
+
+#
+for i in range(10,0,-1):
+    print(i)
+
+
+li = ['FCC','ngx','X']
+for i in range(0,len(li)):
+    ele = li[i]
+    print(ele)
+
+
+
+
+
+
+
+
+
+
