855223577ac0da8af7796213d54496e634eb40a0
x-ning <ngx@ngx.wiki>
2017-06-04 11:23:07 +0800

    正则表达式

---
+++ day06/s1.py
@@ -0,0 +1,18 @@
+#! /usr/bin/env python
+# -*- coding:utf-8 -*-
+# Author: X.Ning <ngx@ngx.wiki>
+import re
+
+# print(re.search('e','alex make love'))
+# print(re.search('^e','alex make love'))
+# print(re.search('e','alex make love'))
+# print(re.match('e','alex make love'))
+# print(re.match('a', 'alex make love').group())
+# print(re.search('al(e)x\smake(?:e)','alex make love').group())
+
+
+
+# print(re.sub('^(\w+)(\s)(\w+)(\s)(\w+)',r'5\4\3\2\1','alex      make      love'))
+# print(re.sub('',r'5\4\3\2\1','alex      make      love'))
+
+print(re.sub('',r' \5\4\3\2\1','alex " \ + = make ----/=    love'))
